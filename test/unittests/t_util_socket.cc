ae1bc8a21c406f5145be37ecd5523c0b41d26c5f
Georgios Christodoulis <christge@cern.ch>
2025-08-15 15:27:29 +0200

    [util] cleanup test suite

--- test/unittests/t_util_socket.cc
+++ test/unittests/t_util_socket.cc
@@ -6,15 +6,42 @@
 #include <set>
 #include <vector>
 
+#include "fd_refcount_mgr.h"  // ::hasher_any
+#include "smallhash.h"
 #include "util/socket.h"
 
-// TODO(christge): The test suit is mature enough to have central setup and
-// cleanup
+class T_IPC_QM : public ::testing::Test {
+ protected:
+  void SetUp() override{
+    shash::Any hash;
+    for (size_t i = 0; i < c0_hash_n; ++i) {
+      hash.Randomize(i);
+      c0_hashes[i] = hash;
+    }
+    for (size_t i = 0; i < c1_hash_n; ++i) {
+      hash.Randomize(i + c0_hash_n);
+      c1_hashes[i] = hash;
+    }
+    const shash::Any hash_null;
+    map_fd_.Init(magic_n, hash_null, ::hasher_any);
+    EXPECT_EQ(map_fd_.size(), 0);
+    for (size_t i = 0; i < map_size; ++i) {
+      hash.Randomize(i + c0_hash_n + c1_hash_n);
+      map_fd_.Insert(hash, i);
+    }
+    EXPECT_EQ(map_fd_.size(), map_size);
+    EXPECT_GE(map_fd_.capacity(), map_size);
+  }
 
+  void TearDown() override { }
+  static constexpr char socket_name[] = "/tmp/test.sock";
+  static constexpr int magic_n = 16;  // TODO(christge): 16 initializes
+                                      // everything in fd_refcount_mgr. Ask WHY?
+  static constexpr size_t c0_hash_n = 250, c1_hash_n = magic_n;
+  shash::Any c0_hashes[c0_hash_n], c1_hashes[c1_hash_n];
+};
 
-TEST(T_IPC_SingleServerSingleClient, ExchangeSingleCommand) {
-  constexpr char socket_name[] = "/tmp/socket-exp/test.sock";
-
+TEST_F(T_IPC_QM, SingleClientExchangeSingleCommand) {
   LocalUnixSocket<ProcessType::Server> server{socket_name};
   LocalUnixSocket<ProcessType::Client> client(socket_name);
 
@@ -30,8 +57,7 @@ TEST(T_IPC_SingleServerSingleClient, ExchangeSingleCommand) {
   EXPECT_EQ(cmd, server.read<util::Command>()[0]);
 }
 
-TEST(T_IPC_SingleServerSingleClient, ExchangeSingleCommandMultiprocess) {
-  constexpr char socket_name[] = "/tmp/socket-exp/test.sock";
+TEST_F(T_IPC_QM, SingleClientExchangeSingleCommandMultiprocess) {
   LocalUnixSocket<ProcessType::Server> server{socket_name};
 
   pid_t pid = fork();
@@ -53,9 +79,7 @@ TEST(T_IPC_SingleServerSingleClient, ExchangeSingleCommandMultiprocess) {
   EXPECT_EQ(util::Command::RecvHashes, server.read<util::Command>()[0]);
 }
 
-TEST(T_IPC_SingleServerSingleClient, ExchangeSingleCommandAsync) {
-  constexpr char socket_name[] = "/tmp/socket-exp/test.sock";
-
+TEST_F(T_IPC_QM, SingleClientExchangeSingleCommandAsync) {
   LocalUnixSocket<ProcessType::Server> server{socket_name};
   LocalUnixSocket<ProcessType::Client> client(socket_name);
 
@@ -78,9 +102,7 @@ TEST(T_IPC_SingleServerSingleClient, ExchangeSingleCommandAsync) {
   EXPECT_EQ(cmd, res[0]);
 }
 
-TEST(T_IPC_SingleServerSingleClient, ExchangeMultipleCommands) {
-  constexpr char socket_name[] = "/tmp/socket-exp/test.sock";
-
+TEST_F(T_IPC_QM, SingleClientExchangeMultipleCommands) {
   LocalUnixSocket<ProcessType::Server> server{socket_name};
   LocalUnixSocket<ProcessType::Client> client(socket_name);
 
@@ -99,9 +121,7 @@ TEST(T_IPC_SingleServerSingleClient, ExchangeMultipleCommands) {
   EXPECT_EQ(result[3], util::Command::RecvHashes);
 }
 
-TEST(T_IPC_SingleServerSingleClient, ExchangeSingleNumber) {
-  constexpr char socket_name[] = "/tmp/socket-exp/test.sock";
-
+TEST_F(T_IPC_QM, SingleClientExchangeSingleNumber) {
   LocalUnixSocket<ProcessType::Server> server{socket_name};
   LocalUnixSocket<ProcessType::Client> client(socket_name);
 
@@ -118,9 +138,7 @@ TEST(T_IPC_SingleServerSingleClient, ExchangeSingleNumber) {
   EXPECT_EQ(number, server.read<int>()[0]);
 }
 
-TEST(T_IPC_SingleServerSingleClient, ExchangeSingleNumberAsync) {
-  constexpr char socket_name[] = "/tmp/socket-exp/test.sock";
-
+TEST_F(T_IPC_QM, SingleClientExchangeSingleNumberAsync) {
   LocalUnixSocket<ProcessType::Server> server{socket_name};
   LocalUnixSocket<ProcessType::Client> client(socket_name);
 
@@ -144,9 +162,7 @@ TEST(T_IPC_SingleServerSingleClient, ExchangeSingleNumberAsync) {
   EXPECT_EQ(number, res[0]);
 }
 
-TEST(T_IPC_SingleServerSingleClient, ExchangeMultipleNumbers) {
-  constexpr char socket_name[] = "/tmp/socket-exp/test.sock";
-
+TEST_F(T_IPC_QM, SingleClientExchangeMultipleNumbers) {
   LocalUnixSocket<ProcessType::Server> server{socket_name};
   LocalUnixSocket<ProcessType::Client> client(socket_name);
 
@@ -162,9 +178,7 @@ TEST(T_IPC_SingleServerSingleClient, ExchangeMultipleNumbers) {
   EXPECT_EQ(result[3], 45);
 }
 
-TEST(T_IPC_SingleServerSingleClient, ExchangeSingleHash) {
-  constexpr char socket_name[] = "/tmp/socket-exp/test.sock";
-
+TEST_F(T_IPC_QM, SingleClientExchangeSingleHash) {
   LocalUnixSocket<ProcessType::Server> server{socket_name};
   LocalUnixSocket<ProcessType::Client> client(socket_name);
 
@@ -178,9 +192,7 @@ TEST(T_IPC_SingleServerSingleClient, ExchangeSingleHash) {
   EXPECT_EQ(hash, server.read<shash::Any>()[0]);
 }
 
-TEST(T_IPC_SingleServerSingleClient, ExchangeSingleHashAsync) {
-  constexpr char socket_name[] = "/tmp/socket-exp/test.sock";
-
+TEST_F(T_IPC_QM, SingleClientExchangeSingleHashAsync) {
   LocalUnixSocket<ProcessType::Server> server{socket_name};
   LocalUnixSocket<ProcessType::Client> client(socket_name);
 
@@ -197,64 +209,43 @@ TEST(T_IPC_SingleServerSingleClient, ExchangeSingleHashAsync) {
   EXPECT_EQ(hash, res[0]);
 }
 
-TEST(T_IPC_SingleServerSingleClient, ExchangeMultipleHashes) {
-  constexpr char socket_name[] = "/tmp/socket-exp/test.sock";
-
+TEST_F(T_IPC_QM, SingleClientExchangeMultipleHashes) {
   LocalUnixSocket<ProcessType::Server> server{socket_name};
   LocalUnixSocket<ProcessType::Client> client(socket_name);
-
-  constexpr size_t N = 250;
-  std::vector<shash::Any> hashes;
-  for (size_t i = 0; i < N; ++i) {
-    shash::Any hash;
-    hash.Randomize(i);
-    hashes.emplace_back(hash);
-  }
 
   client.connect();
   server.accept();
 
-  for (size_t i = 0; i < N; ++i) {
-    client.write(hashes[i]);
+  for (size_t i = 0; i < c0_hash_n; ++i) {
+    client.write(c0_hashes[i]);
   }
-  std::vector<shash::Any> result = server.read<shash::Any>(N);
-  for (size_t i = 0; i < N; ++i) {
-    EXPECT_EQ(hashes[i], result[i]);
+  std::vector<shash::Any> result = server.read<shash::Any>(c0_hash_n);
+  for (size_t i = 0; i < c0_hash_n; ++i) {
+    EXPECT_EQ(c0_hashes[i], result[i]);
   }
 }
 
-TEST(T_IPC_SingleServerSingleClient, ExchangeMultipleHashesAsync) {
-  constexpr char socket_name[] = "/tmp/socket-exp/test.sock";
-
+TEST_F(T_IPC_QM, SingleClientExchangeMultipleHashesAsync) {
   LocalUnixSocket<ProcessType::Server> server{socket_name};
   LocalUnixSocket<ProcessType::Client> client(socket_name);
-
-  constexpr size_t N = 250;
-  std::vector<shash::Any> hashes;
-  for (size_t i = 0; i < N; ++i) {
-    shash::Any hash;
-    hash.Randomize(i);
-    hashes.emplace_back(hash);
-  }
 
   client.connect();
   server.accept();
 
-  EXPECT_EQ(0, server.try_read<shash::Any>(N).size());
+  EXPECT_EQ(0, server.try_read<shash::Any>(c0_hash_n).size());
 
-  for (size_t i = 0; i < N; ++i) {
-    client.write(hashes[i]);
+  for (size_t i = 0; i < c0_hash_n; ++i) {
+    client.write(c0_hashes[i]);
   }
-  std::vector<shash::Any> result = server.try_read<shash::Any>(N);
+  std::vector<shash::Any> result = server.try_read<shash::Any>(c0_hash_n);
 
-  EXPECT_EQ(N, result.size());
-  for (size_t i = 0; i < N; ++i) {
-    EXPECT_EQ(hashes[i], result[i]);
+  EXPECT_EQ(c0_hash_n, result.size());
+  for (size_t i = 0; i < c0_hash_n; ++i) {
+    EXPECT_EQ(c0_hashes[i], result[i]);
   }
 }
 
-TEST(T_IPC_SingleServerMultipleClients, ExchangeSingleCommand) {
-  constexpr char socket_name[] = "/tmp/socket-exp/test.sock";
+TEST_F(T_IPC_QM, MultipleClientsExchangeSingleCommand) {
   LocalUnixSocket<ProcessType::Server> server{socket_name};
   LocalUnixSocket<ProcessType::Client> c0(socket_name);
   LocalUnixSocket<ProcessType::Client> c1(socket_name);
@@ -275,8 +266,7 @@ TEST(T_IPC_SingleServerMultipleClients, ExchangeSingleCommand) {
   EXPECT_EQ(recv, c1.read<util::Command>()[0]);
 }
 
-TEST(T_IPC_SingleServerMultipleClients, ExchangeSingleNumber) {
-  constexpr char socket_name[] = "/tmp/socket-exp/test.sock";
+TEST_F(T_IPC_QM, MultipleClientsExchangeSingleNumber) {
   LocalUnixSocket<ProcessType::Server> server{socket_name};
   LocalUnixSocket<ProcessType::Client> c0(socket_name);
   LocalUnixSocket<ProcessType::Client> c1(socket_name);
@@ -295,8 +285,7 @@ TEST(T_IPC_SingleServerMultipleClients, ExchangeSingleNumber) {
   EXPECT_EQ(24, server.read<int>(1, 1)[0]);
 }
 
-TEST(T_IPC_SingleServerMultipleClients, ExchangeSingleHash) {
-  constexpr char socket_name[] = "/tmp/socket-exp/test.sock";
+TEST_F(T_IPC_QM, MultipleClientsExchangeSingleHash) {
   LocalUnixSocket<ProcessType::Server> server{socket_name};
   LocalUnixSocket<ProcessType::Client> c0(socket_name);
   LocalUnixSocket<ProcessType::Client> c1(socket_name);
@@ -320,8 +309,7 @@ TEST(T_IPC_SingleServerMultipleClients, ExchangeSingleHash) {
   EXPECT_EQ(hashes[1], server.read<shash::Any>(1, 1)[0]);
 }
 
-TEST(T_IPC_SingleServerMultipleClients, RealisticCase) {
-  constexpr char socket_name[] = "/tmp/socket-exp/test.sock";
+TEST_F(T_IPC_QM, MultipleClientsRealisticCase) {
   LocalUnixSocket<ProcessType::Server> server{socket_name};
   LocalUnixSocket<ProcessType::Client> c0(socket_name);
   LocalUnixSocket<ProcessType::Client> c1(socket_name);
@@ -344,14 +332,9 @@ TEST(T_IPC_SingleServerMultipleClients, RealisticCase) {
 
   EXPECT_EQ(util::Command::RecvHashes, server.read<util::Command>(1, 0)[0]);
   EXPECT_EQ(util::Command::RecvHashes, server.read<util::Command>(1, 1)[0]);
-  constexpr size_t c0_hash_n = 42, c1_hash_n = 35;
-  shash::Any c0_hashes[c0_hash_n], c1_hashes[c1_hash_n];
-  shash::Any hash;
 
   for (size_t i = 0; i < c0_hash_n; ++i) {
-    hash.Randomize(i);
-    c0_hashes[i] = hash;
-    c0.write(hash);
+    c0.write(c0_hashes[i]);
   }
   std::vector<shash::Any> c0_res = server.read<shash::Any>(c0_hash_n, 0);
   for (size_t i = 0; i < c0_hash_n; ++i) {
@@ -359,9 +342,7 @@ TEST(T_IPC_SingleServerMultipleClients, RealisticCase) {
   }
 
   for (size_t i = 0; i < c1_hash_n; ++i) {
-    hash.Randomize(i);
-    c1_hashes[i] = hash;
-    c1.write(hash);
+    c1.write(c1_hashes[i]);
   }
   std::vector<shash::Any> c1_res = server.read<shash::Any>(c1_hash_n, 1);
   for (size_t i = 0; i < c1_hash_n; ++i) {
@@ -369,8 +350,7 @@ TEST(T_IPC_SingleServerMultipleClients, RealisticCase) {
   }
 }
 
-TEST(T_IPC_SingleServerMultipleClients, RealisticCaseAsync) {
-  constexpr char socket_name[] = "/tmp/socket-exp/test.sock";
+TEST_F(T_IPC_QM, MultipleClientsRealisticCaseAsync) {
   LocalUnixSocket<ProcessType::Server> server{socket_name};
   LocalUnixSocket<ProcessType::Client> c0(socket_name);
   LocalUnixSocket<ProcessType::Client> c1(socket_name);
@@ -417,9 +397,6 @@ TEST(T_IPC_SingleServerMultipleClients, RealisticCaseAsync) {
   // SERVER receives the response
   EXPECT_EQ(util::Command::RecvHashes, server.try_read<util::Command>(1, 0)[0]);
   EXPECT_EQ(util::Command::RecvHashes, server.try_read<util::Command>(1, 1)[0]);
-  constexpr size_t c0_hash_n = 42, c1_hash_n = 35;
-  shash::Any c0_hashes[c0_hash_n], c1_hashes[c1_hash_n];
-  shash::Any hash;
 
   // Meanwhile SERVER is waiting for the hashes
   EXPECT_EQ(0, server.try_read<shash::Any>(c0_hash_n, 0).size());
@@ -431,9 +408,7 @@ TEST(T_IPC_SingleServerMultipleClients, RealisticCaseAsync) {
 
   // c0 SENDS its hashes
   for (size_t i = 0; i < c0_hash_n; ++i) {
-    hash.Randomize(i);
-    c0_hashes[i] = hash;
-    c0.write(hash);
+    c0.write(c0_hashes[i]);
   }
 
   // SERVER gets the hashes of c0, while waiting for the hashes of c1
@@ -448,9 +423,7 @@ TEST(T_IPC_SingleServerMultipleClients, RealisticCaseAsync) {
 
   // SERVER gets the hashes of c1
   for (size_t i = 0; i < c1_hash_n; ++i) {
-    hash.Randomize(i);
-    c1_hashes[i] = hash;
-    c1.write(hash);
+    c1.write(c1_hashes[i]);
   }
   std::vector<shash::Any> c1_res = server.try_read<shash::Any>(c1_hash_n, 1);
   EXPECT_EQ(c1_hash_n, c1_res.size());
@@ -459,21 +432,8 @@ TEST(T_IPC_SingleServerMultipleClients, RealisticCaseAsync) {
   }
 }
 
-TEST(T_IPC_QuotaCacheMgrsCommunication, CollectHashes) {
-  constexpr char socket_name[] = "/tmp/socket-exp/test.sock";
+TEST_F(T_IPC_QM, CollectHashes) {
   QuotaManagerSocket qm{socket_name};
-
-  constexpr size_t cm0_n = 42, cm1_n = 35;
-  shash::Any hash;
-  shash::Any cm0_hashes[cm0_n], cm1_hashes[cm1_n];
-  for (size_t i = 0; i < cm0_n; ++i) {
-    hash.Randomize(i);
-    cm0_hashes[i] = hash;
-  }
-  for (size_t i = 0; i < cm1_n; ++i) {
-    hash.Randomize(i);
-    cm1_hashes[i] = hash;
-  }
 
   pid_t pid = fork();
   // handshake to establish communication with each CacheManager
@@ -492,31 +452,88 @@ TEST(T_IPC_QuotaCacheMgrsCommunication, CollectHashes) {
       EXPECT_TRUE(cmd.size() > 0);
       EXPECT_EQ(cmd[0], util::Command::SendHashes);
       cm0.write(util::Command::RecvHashes);
-      cm0.write(cm0_n);
-      for (size_t i = 0; i < cm0_n; ++i) {
-        cm0.write(cm0_hashes[i]);
+      cm0.write(c0_hash_n);
+      for (size_t i = 0; i < c0_hash_n; ++i) {
+        cm0.write(c0_hashes[i]);
       }
 
       cmd = cm1.read<util::Command>();
       EXPECT_TRUE(cmd.size() > 0);
       EXPECT_EQ(cmd[0], util::Command::SendHashes);
       cm1.write(util::Command::RecvHashes);
-      cm1.write(cm1_n);
-      for (size_t i = 0; i < cm1_n; ++i) {
-        cm0.write(cm1_hashes[i]);
+      cm1.write(c1_hash_n);
+      for (size_t i = 0; i < c1_hash_n; ++i) {
+        cm0.write(c1_hashes[i]);
       }
-
       _exit(0);
   }
   // QuotaManager code
   qm.accept();
   // asking for hashes
   std::set<shash::Any> collected = qm.collect<shash::Any>();
-  for (size_t i = 0; i < cm0_n; ++i) {
-    EXPECT_TRUE(collected.find(cm0_hashes[i]) != collected.end());
+  for (size_t i = 0; i < c0_hash_n; ++i) {
+    EXPECT_TRUE(collected.find(c0_hashes[i]) != collected.end());
   }
-  for (size_t i = 0; i < cm1_n; ++i) {
-    EXPECT_TRUE(collected.find(cm1_hashes[i]) != collected.end());
+  for (size_t i = 0; i < c1_hash_n; ++i) {
+    EXPECT_TRUE(collected.find(c1_hashes[i]) != collected.end());
+  }
+}
+
+TEST_F(T_IPC_QM, CollectHashesCCZero) {
+  QuotaManagerSocket qm{socket_name};
+
+  pid_t pid = fork();
+  // handshake to establish communication with each CacheManager
+  switch (pid) {
+    case -1:
+      ASSERT_TRUE(false);
+      break;
+    case 0:
+      // CacheManager-s code
+      CacheManagerSocket cm0{socket_name};
+      cm0.connect();
+
+      auto cmd = cm0.read<util::Command>();
+      EXPECT_TRUE(cmd.size() > 0);
+      EXPECT_EQ(cmd[0], util::Command::SendHashes);
+      cm0.write(util::Command::RecvHashes);
+      cm0.write(0);
+      _exit(0);
+  }
+  // QuotaManager code
+  qm.accept();
+  // asking for hashes
+  std::set<shash::Any> collected = qm.collect<shash::Any>();
+  EXPECT_EQ(collected.size(), 0);
+}
+
+TEST_F(T_IPC_QM, SingleClientSendSmallHashDynamicUseAPI) {
+  QuotaManagerSocket qm{socket_name};
+
+  pid_t pid = fork();
+  // handshake to establish communication with each CacheManager
+  switch (pid) {
+    case -1:
+      ASSERT_TRUE(false);
+      break;
+    case 0:
+      // CacheManager-s code
+      CacheManagerSocket cm0{socket_name};
+      cm0.connect();
+
+      auto cmd = cm0.read<util::Command>();
+      EXPECT_TRUE(cmd.size() > 0);
+      EXPECT_EQ(cmd[0], util::Command::SendHashes);
+      cm0.send_hashes(map_fd_);
+      _exit(0);
+  }
+  // QuotaManager code
+  qm.accept();
+  std::set<shash::Any> collected = qm.collect<shash::Any>();
+  EXPECT_EQ(collected.size(), map_size);
+  for (const auto &hash : collected) {
+    int value;
+    EXPECT_TRUE(map_fd_.Lookup(hash, &value));
   }
 }
 
