265790a238ba191c6393d7314c729e70d4a0d134
GeorgeChr <g.chr.personal@posteo.net>
2026-08-11 17:44:17 +0200

    Replace memory fence with std::atomic_thread_fence

--- cvmfs/cache_extern.cc
+++ cvmfs/cache_extern.cc
@@ -16,6 +16,7 @@
 #ifdef __APPLE__
 #include <cstdlib>
 #endif
+#include <atomic>
 #include <cstring>
 #include <map>
 #include <new>
@@ -24,7 +25,6 @@
 
 #include "cache.pb.h"
 #include "crypto/hash.h"
-#include <atomic>
 #include "util/concurrency.h"
 #include "util/exception.h"
 #include "util/logging.h"
@@ -402,7 +402,7 @@ ExternalCacheManager::ExternalCacheManager(int fd_connection,
 
 ExternalCacheManager::~ExternalCacheManager() {
   terminated_ = true;
-  MemoryFence();
+  std::atomic_thread_fence(std::memory_order_seq_cst);
   if (session_id_ >= 0) {
     cvmfs::MsgQuit msg_quit;
     msg_quit.set_session_id(session_id_);
